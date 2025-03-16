055bb7d956d18617ce48b7da07341ead73c95ff1
Kevin Djabaku Ocansey <kevinocansey11@gmail.com>
2025-03-16 01:03:04 +0000

    update installed APPs

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #  
     'patients',
     'medical_history',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
